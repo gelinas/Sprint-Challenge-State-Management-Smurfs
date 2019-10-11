12b3f74fea913b60b158a8f4525d616e8bc4e5ae
gelinas <jlgelinas@gmail.com>
2019-10-11 12:50:23 -0400

    classname error fixed to className

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
 const Smurf = props => {
-  console.log("props from Smurf", props);
   return (
     <div className="smurf-card">
-      <div classname="smurf-data">
-        <p>{`Smurf: ${props.smurf.name}`}</p>
-        <p>{`Age: ${props.smurf.age}`}</p>
-        <p>{`Height: ${props.smurf.height}`}</p>
-      </div>
+      <p>{`Smurf: ${props.smurf.name}`}</p>
+      <p>{`Age: ${props.smurf.age}`}</p>
+      <p>{`Height: ${props.smurf.height}`}</p>
     </div>
   )
 };
